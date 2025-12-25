e91831b28bb84da8c86c94cddfd39f94b82b430a
Banan41ke <kolybrigadin@gmail.com>
2025-12-25 17:06:08 +0300

    Add PostgreSQL support for Render

--- movie_reviews_website_project/settings.py
+++ movie_reviews_website_project/settings.py
@@ -1,5 +1,6 @@
 # movie_reviews_website_project/settings.py
-
+import os
+import dj_database_url
 from pathlib import Path
 from decouple import config
 
@@ -56,10 +57,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'movie_reviews_website_project.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
